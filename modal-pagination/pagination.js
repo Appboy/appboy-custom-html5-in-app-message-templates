6ac60026f4d7ea26a3f78644f93952c4b060712c
stella kim <stellajkimm@gmail.com>
2017-03-13 16:53:45 -0400

    Fixed bug with footer buttons in pagination templates

--- modal-pagination/pagination.js
+++ modal-pagination/pagination.js
@@ -14,24 +14,23 @@ $(document).ready(function(){
     }
   });
 
-  // This event listener changes the footer buttons.  When the modal reaches the last slide, it changes the buttons to "GOT IT!"
-  // Otherwise, the footer has buttons "SKIP" and "NEXT"
-  $('.slick-container').on('beforeChange', function(event, slick, direction) {
-    var footer;
+  // Initially hide the "GOT IT" button until you reach the last slide
+  $('.button-center').hide();
+  // This event listener changes the footer buttons.  When the modal reaches the last slide, 
+  // it hides the "SKIP" and "NEXT" buttons and displays the "GOT IT!" button
+  $('.slick-container').on('beforeChange', function(event, slick, currentSlide, nextSlide) {
+    var lastSlide = slick.slideCount-1
+    var secondToLastSlide = slick.slideCount-2
     if (slick.slideCount >= 2) {
-      if (slick.currentSlide === slick.slideCount-2 && slick.currentDirection == 0) {
-        footer = "<div class='button-center'>"+
-                   "<a href='appboy://close'><p>GOT IT!</p></a>"+
-                 "</div>"
-      } else if (slick.currentSlide === slick.slideCount-1 && slick.currentDirection == 1) {
-        footer = "<div class='button-left'>"+
-                   "<a href='appboy://close'><p>SKIP</p></a>"+
-                 "</div>"+
-                 "<div class='button-right'>"+
-                   "<a href=''><p>NEXT</p></a>"+
-                 "</div>"
+      if (currentSlide === secondToLastSlide && nextSlide == lastSlide) {
+        $('.button-left').hide();
+        $('.button-right').hide();
+        $('.button-center').show();
+      } else if (currentSlide === lastSlide && nextSlide == secondToLastSlide) {
+        $('.button-left').show();
+        $('.button-right').show();
+        $('.button-center').hide();
       };
-      $('footer').html(footer);
     };
-  })
+  });
 });
